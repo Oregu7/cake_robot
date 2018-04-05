7c92d96d90d9bb02f28a5ca7d7ce02e6015de14d
Oregu7 <jekkicool@yandex.ru>
2018-04-05 15:12:02 +0300

    deploy@0.1

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
+const fs = require("fs");
+const ip = require("ip");
+const config = require("config");
+
+const env = config.get("env");
+const port = config.get("server.port");
+const token = config.get("bot.token");
 const bot = require("./app/bot");
-const server = require("./app/server");
 //start bot
-bot.startPolling();
-//server.listen(3000);
+if (env === "development") {
+    bot.telegram.setWebhook();
+    bot.startPolling();
+} else {
+    // TLS options
+    const tlsOptions = {
+        key: fs.readFileSync("./webhook_pkey.pem"),
+        cert: fs.readFileSync("./webhook_cert.pem"),
+    };
+
+    // Set telegram webhook
+    bot.telegram.setWebhook(`https://${ip.address()}:${port}/bot-${token}`, {
+        source: fs.readFileSync("./webhook_cert.pem"),
+    });
+
+    // Start https webhook
+    bot.startWebhook(`/bot-${token}`, tlsOptions, port);
+}
